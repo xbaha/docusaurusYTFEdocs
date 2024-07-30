5a8f9f7fb92a011201f7c5ffbd4ac5c705e82b85
azszero <xbaha@hotmail.com>
2024-07-30 13:01:15 -0700

    Update move_to_docs.sh

--- move_to_docs.sh
+++ move_to_docs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Ensure the build/docs directory does not already exist
-rm -rf build/docs
+rm -rf build/documents
 
 # Create the docs directory inside build
-mkdir -p build/docs
+mkdir -p build/documents
 
 # Move all contents of build to build/docs
 shopt -s dotglob  # Include hidden files and directories
-mv build/* build/docs/
+mv build/* build/documents/
 shopt -u dotglob  # Disable the dotglob option
